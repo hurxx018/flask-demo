4704e20797baac58f6abf31d8117aadd55092c1e
hurxx018 <hurxx018@gmail.com>
2016-05-23 18:01:25 -0500

    Update a html file.

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+app.vars = {}
+
 @app.route('/')
 def main():
   return redirect('/index')
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+  return render_template('index.html', question='Ticker symbol: ')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run(port=33507, debug=True)
+  # debug is disabled.
+  # app.run(host='0.0.0.0') # The operating system listens on all public IPs.
